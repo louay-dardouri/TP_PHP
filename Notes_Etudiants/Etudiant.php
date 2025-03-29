dae2d09c72174fe58e801a41f49ed887839a1010
ghassen naouar <ghassennaouar7@gmail.com>
2025-03-29 20:46:10 +0100

    cleaning code

--- Notes_Etudiants/Etudiant.php
+++ Notes_Etudiants/Etudiant.php
@@ -1,10 +1,8 @@
 <?php 
 class Etudiant{
-    // private attributes for encapsulation
     private $nom;
     private $notes = []; 
 
-    //constructor
     public function __construct($nom,$notes){
         $this->nom = $nom;
         $this->notes = $notes;
@@ -13,7 +11,7 @@ class Etudiant{
     }
 
     
-    //methode treturny moyenne
+ 
     public function calculMoyenne(){
         if(count($this->notes) === 0){
             return 0;
@@ -24,21 +22,6 @@ class Etudiant{
 
     }
 
-    //methode bch taffichy note lil etudiant
-    /* the table block:
-    <table>
-        <tr>
-            <th>header of col1</th>
-            <th>header of col2</th>
-
-        </tr>
-        <tr>
-            <th>value for col1</th>
-            <th>value for col2</th>
-        </tr>
-    </table>
-    
-    */
     public function afficheNotes(){
         echo "<div class='card shadow-sm'>";
         echo "<div class='card-header text-center fw-bold'>" . htmlspecialchars($this->nom) . "</div>";
@@ -51,7 +34,7 @@ class Etudiant{
         echo "<li class='list-group-item text-center bg-primary text-white'>Votre moyenne est $moy</li>";
         echo "</ul></div>";
     }
-    //methode estAdmis
+
     public function estAdmis(){
         $moy = $this->CalculMoyenne();
         if($moy >= 10) echo "admis";
