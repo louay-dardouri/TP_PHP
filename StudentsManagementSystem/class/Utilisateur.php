e7fe5899db75ccb364ca6f1a6deb83469f115a6a
Ahmed Loubiri <ahmed.loubiri@insat.ucar.tn>
2025-04-03 20:31:37 +0100

    Updated Utilisateur

--- StudentsManagementSystem/class/Utilisateur.php
+++ StudentsManagementSystem/class/Utilisateur.php
@@ -1,10 +1,6 @@
 <?php
 include_once 'ConnexionDB.php';
 class Utilisateur{
-    private $_id;
-    private $_username;
-    private $_email;
-    private $_role;
     private $_conn;
     private $_table = "utilisateur";
 
@@ -18,7 +14,31 @@ class Utilisateur{
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         return ($row && $row['role'] == 'admin');
     }
-    
+    public function addEtudiant($id, $name, $birthday, $image, $section): int {
+        if (!$this->isAdmin($id)) {
+            return -1;
+        }
+        $query = "INSERT INTO etudiant (name, birthday, image, section) VALUES (:name, :birthday, :image, :section)";
+        $res = $this->_conn->prepare($query);
+        $res->execute(['name' => $name, 'section' => $section, 'birthday' => $birthday, 'image' => $image]);
+        return $this->_conn->lastInsertId();
+    }
+    public function updateEtudiant($id_user, $id, $name, $birthday, $image, $section): int {
+        if (!$this->isAdmin($id_user)) {
+            return -1;
+        }
+        $query = "UPDATE etudiant SET name = :name, birthday = :birthday, image = :image, section = :section WHERE id = :id";
+        $res = $this->_conn->prepare($query);
+        $res->execute(['id' => $id, 'name' => $name,'birthday' => $birthday, 'image' => $image, 'section' => $section]);
+        return $res->rowCount();
+    }
+    public function deleteEtudiant($id_user, $id): int {
+        if (!$this->isAdmin($id_user)) {
+            return -1;
+        }
+        $query = "DELETE FROM etudiant WHERE id = :id";
+        $res = $this->_conn->prepare($query);
+        $res->execute(['id' => $id]);
+        return $res->rowCount();
+    }
 }
-
-
