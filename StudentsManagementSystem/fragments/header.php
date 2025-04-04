9746e6c421c9e3299a773e62c76447a7b3f70e67
Ahmed Loubiri <ahmed.loubiri@insat.ucar.tn>
2025-04-04 17:57:36 +0100

    addEtudiant php file

--- StudentsManagementSystem/fragments/header.php
+++ StudentsManagementSystem/fragments/header.php
@@ -7,6 +7,7 @@
     <?php if (isset($cssPath)) {
         echo "<link rel='stylesheet' href= '$cssPath'> ";
     }?>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <title>
       <?php echo $pageTitle ?? 'Students Management System'; ?>
     </title>
