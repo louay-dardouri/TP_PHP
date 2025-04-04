e68df42a345062aaf2350dd5bcfb0bf9b4983dcb
louay-dardouri <louay.dardouri@insat.ucar.tn>
2025-04-04 13:31:32 +0100

    login system

--- StudentsManagementSystem/home.php
+++ StudentsManagementSystem/home.php
@@ -1,11 +1,6 @@
 <?php
 include_once 'fragments/header.php';
-
-session_start();
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit();
-}
+include_once 'isAuthentificated.php';
 
 echo '<h1>Hello, PHP LOVERS! Welcome to your administration Platform</h1>';
 echo "<h2>Welcome, " . $_SESSION['username'] . "!</h2>";
