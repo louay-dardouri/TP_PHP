5a16e7a85d83728f628d0204361b1ac3afe1ed3b
louay-dardouri <louay.dardouri@insat.ucar.tn>
2025-04-06 14:44:00 +0100

    repo delete method

--- StudentsManagementSystem/class/Repository.php
+++ StudentsManagementSystem/class/Repository.php
@@ -1,10 +1,14 @@
 <?php
 
-abstract class Repository implements IRepository{
-    private $_conn;
-    private $_table;
+include_once 'IRepository.php';
 
-    public function __construct($db,$tableName)
+abstract class Repository implements IRepository
+{
+    protected $_conn;
+
+    protected $_table;
+
+    public function __construct($db, $tableName)
     {
         $this->_table = $tableName;
         $this->_conn = $db;
@@ -12,15 +16,26 @@ abstract class Repository implements IRepository{
 
     public function findAll()
     {
-        $query = "SELECT * FROM $this->_table";
+        $query = 'SELECT * FROM '.$this->_table;
         $res = $this->_conn->query($query);
+
         return $res->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    public function findById(int $id){
+    public function findById(int $id)
+    {
         $stmt = $this->_conn->prepare("SELECT * FROM {$this->_table} WHERE id = :id");
         $stmt->execute([':id' => $id]);
         $res = $stmt->fetch(PDO::FETCH_ASSOC);
+
         return $res ? $res : null;
     }
+
+    public function delete($id)
+    {
+        $query = 'delete * from '.$this->tableName.' where id = :id';
+        $response = $this->_conn->prepare($query);
+        $response->execute(['id' => $id]);
+    }
 }
+
