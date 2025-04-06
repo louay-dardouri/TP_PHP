d13042d7f241ff8b90602cf457f7eba1bcad4081
louay-dardouri <louay.dardouri@insat.ucar.tn>
2025-04-06 13:46:20 +0100

    export to pdf for etudiants and sections

---
+++ StudentsManagementSystem/exportPDF.php
@@ -0,0 +1,12 @@
+<?php
+
+include_once 'vendor/autoload.php';
+include_once 'isAuthentificated.php';
+
+ob_start();
+include './strippedListEtudiant.php';
+$htmlContent = ob_get_clean();
+
+$mpdf = new \Mpdf\Mpdf;
+$mpdf->WriteHTML($htmlContent);
+$mpdf->Output('webpage.pdf', 'I');
